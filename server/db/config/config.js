b9a2f7bf3af5322e9ab54554f9105f2028a064fc
cvjude <jjchinosoviolet@gmail.com>
2021-01-07 13:02:39 +0100

    modify

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -4,14 +4,41 @@ dotenv.config();
 
 module.exports = {
   development: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'DATABASE_URL'
   },
   test: {
     use_env_variable: 'TEST_DATABASE_URL',
-    logging: false,
+    logging: false
   },
   production: {
     use_env_variable: 'DATABASE_URL',
-    logging: false,
+    logging: false
   }
 };
+
+// {
+//   "development": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_development",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql",
+//     "operatorsAliases": false
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql",
+//     "operatorsAliases": false
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql",
+//     "operatorsAliases": false
+//   }
+// }
